c60bdbfbab3d2d8250578a7a29c1d8455fc2da75
Ra1n-42 <150204147+Ra1n-42@users.noreply.github.com>
2024-05-29 15:18:40 +0200

    404 page fixed

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +8,5 @@ export default defineConfig({
     host: true,
     strictPort: true,
     // port: 8080,
-   
-  }
-})
+  },
+});
