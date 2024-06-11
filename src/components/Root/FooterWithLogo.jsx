414156b216fb9299f57d6af7e4395cb0ee655d5c
Ra1n-42 <150204147+Ra1n-42@users.noreply.github.com>
2024-06-11 14:26:43 +0200

    add under construction banner/alert

--- src/components/Root/FooterWithLogo.jsx
+++ src/components/Root/FooterWithLogo.jsx
@@ -1,8 +1,35 @@
 import { Typography } from "@material-tailwind/react";
+import { Alert } from "@material-tailwind/react";
+
+function Icon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={2}
+      stroke="currentColor"
+      className="h-6 w-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+      />
+    </svg>
+  );
+}
 
 export function FooterWithLogo() {
   return (
     <footer className="w-full bg-white p-8">
+      <Alert
+        className="fixed left-0 bottom-0.5 flex justify-center"
+        // variant="outlined"
+        icon={<Icon />}
+      >
+        UNDER CONSTRUCTION
+      </Alert>
       <div className="flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 bg-white text-center md:justify-between">
         <div></div>
         <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
